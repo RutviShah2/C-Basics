812ff8aa83b114e9dc9c7986c5b81af1598d70a1
RutviShah2 <shahrutvi020@gmail.com>
2025-11-01 10:39:08 +0530

    pointers basic added

---
+++ Pointers/basics1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+ {
+    int a = 10;
+    int *ptr = &a; 
+    printf("Value of a: %d\n", a);
+    printf("Address of a: %p\n", &a);
+    printf("Pointer ptr stores: %p\n", ptr);
+    printf("Value using pointer: %d\n", *ptr); 
+    return 0;
+}
