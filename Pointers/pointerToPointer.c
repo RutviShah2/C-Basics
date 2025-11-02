e51bb87fb12726c422e9a0ed258bbddaf7afff09
RutviShah2 <shahrutvi020@gmail.com>
2025-11-02 09:30:29 +0530

    added

---
+++ Pointers/pointerToPointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x = 10;
+    int *p = &x;
+    int **q = &p;
+    printf("x = %d\n", x);
+    printf("*p = %d\n", *p);
+    printf("**q = %d\n", **q);
+    return 0;
+}
