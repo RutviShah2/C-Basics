ed744b86feda2432042dda1a55f8be760fc328c8
RutviShah2 <shahrutvi020@gmail.com>
2025-11-20 21:39:27 +0530

    initial commit

---
+++ Loops/primeNumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=2; i<=50; i++)
+    {
+        int isPrime = 1;
+        for(int j=2; j<=i/2; j++)
+        {
+            if(i % j == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+        if(isPrime)
+            printf("%d ", i);
+    }
+}
